62ab8c613ed0269c4636289afc9f66d30fc96017
Paul Buzakov <paulbuzakov@gmail.com>
2024-05-10 14:15:37 +0500

    feat: #3 Add some short commands
    refs:
    https://github.com/paulbuzakov/pwshconfig/issues/3

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,15 @@
-Set-Alias -Name powershell	-Value pwsh
-Set-Alias -Name g		-Value git
-Set-Alias -Name v		-Value nvim
-Set-Alias -Name d		-Value dotnet
+# Short commands used by the system and
+# possible typos
+Set-Alias -Name powershell -Value pwsh
+Set-Alias -Name vi         -Value nvim
+Set-Alias -Name vim        -Value nvim
+
+# Short command used in my workspaces
+Set-Alias -Name p -Value pwsh
+Set-Alias -Name g -Value git
+Set-Alias -Name v -Value nvim
+Set-Alias -Name d -Value dotnet
+Set-Alias -Name t -Value tmux
 
 if ($IsMacOS) {
   Import-Module -Name $PSScriptRoot\macos.ps1
